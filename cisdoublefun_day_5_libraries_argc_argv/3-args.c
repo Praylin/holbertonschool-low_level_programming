70c373b722211b511cfc932b15f8d6087d859e1e
Praylin <praylin.diana@gmail.com>
2016-05-02 06:02:34 +0000

    Removed header files

--- cisdoublefun_day_5_libraries_argc_argv/3-args.c
+++ cisdoublefun_day_5_libraries_argc_argv/3-args.c
@@ -1,13 +1,26 @@
 /*Print all the arguments*/
-#include <stdio.h>
+#include <unistd.h>
+int print_char(char c);
 int main(int argc, char *argv[])
 {
-  int i = 0;
+  int i = 0, j = 0;
+  char *str;
   while ( i < argc)
     {
-      printf("%s", argv[i]);
-      printf("\n");
-      i++;
+      str = argv[i];
+j = 0;
+      while (str[j] != '\0')
+	{
+	  print_char(str[j]);
+	  j++;
+	}
+	print_char('\n');
+       i++;
     }
   return 0;
+}
+
+int print_char(char c)
+{
+return (write(1, &c, 1));
 }
--- cisdoublefun_day_5_libraries_argc_argv/4-args.c
+++ cisdoublefun_day_5_libraries_argc_argv/4-args.c
@@ -1,14 +1,27 @@
 /*Print all the arguments in reverse order*/
-#include <stdio.h>
+#include <unistd.h>
+int print_char(char c);
 int main(int argc, char *argv[])
 {
-  int i;
+  int i, j = 0;
+  char *str;
   i = (argc - 1);
   while ( i >= 0)
     {
-      printf("%s", argv[i]);
-      printf("\n");
+      str = argv[i];
+      j = 0;
+      while (str[j] != '\0')
+        {
+          print_char(str[j]);
+          j++;
+        }
+      print_char('\n');
       i--;
     }
   return 0;
+}
+
+int print_char(char c)
+{
+  return (write(1, &c, 1));
 }
