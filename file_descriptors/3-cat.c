73435222153fe7ac199b41f3448c39d2aec80636
Praylin <praylin.diana@gmail.com>
2016-05-10 16:16:57 +0000

    Basic Cat command function with C

--- file_descriptors/3-cat.c
+++ file_descriptors/3-cat.c
@@ -17,41 +17,40 @@ int main(int ac, char **av)
   
   if(ac > 1)
   {
-    for(i = 0; i < ac; i++)
+    for(i = 1; i < ac; i++)
       {
-    filename = av[i];
-    count = 1;
-    fd = open(filename, O_RDONLY); /*Opening file*/
-    if(fd == -1)
-      {
-	perror("Open");
-	return 1;
-      }
-    count = (int)file_size(filename); /*Finding size of file*/
-    buffer = malloc(count * sizeof(char)); /*Allocate sufficient space in memory for the content of file*/
-    if(buffer == NULL)
-      return 1;
-    buffer[count + 1] = '\0';
-    rd = read(fd, buffer, count); /*Read file*/
-    if(rd == -1)
-      {
-	perror("Read");
-	return 1;
-      }
-    wr = write(1, buffer, count+1);
-    if(wr == -1)
-      {
-	perror("Write");
-	return 1;
-      }
-    free(buffer);
-    i++;
+	filename = av[i];
+	count = 1;
+	fd = open(filename, O_RDONLY); /*Opening file*/
+	if(fd == -1)
+	  {
+	    perror("Open");
+	    return 1;
+	  }
+	count = (int)file_size(filename); /*Finding size of file*/
+	buffer = malloc(count * sizeof(char)); /*Allocate sufficient space in memory for the content of file*/
+	if(buffer == NULL)
+	  return 1;
+	buffer[count + 1] = '\0';
+	rd = read(fd, buffer, count); /*Read file*/
+	if(rd == -1)
+	  {
+	    perror("Read");
+	    return 1;
+	  }
+	wr = write(1, buffer, count+1);
+	if(wr == -1)
+	  {
+	    perror("Write");
+	    return 1;
+	  }
       }
   }
   else
     return 1;
-   close(fd);
-   return (0);
+  close(fd);
+  free(buffer);
+  return (0);
 }
 
   /*Function that returns the size of a file in type long.*/
